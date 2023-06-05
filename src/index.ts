eaf8292e75a0f9d092f96a5db1d06f0edd61ac92
fengmk2 <fengmk2@gmail.com>
2023-06-05 13:38:07 +0800

    feat: refactor with typescript
    BREAKING CHANGE: Drop Node.js < 16.13.0 support
    
    https://github.com/eggjs/egg-core/issues/264

---
+++ src/index.ts
@@ -0,0 +1,81 @@
+export type CallbackFunction = (err?: Error) => void;
+export type ReadyFunctionArg = boolean | Error | CallbackFunction | undefined;
+
+export default class Ready {
+  #isReady: boolean;
+  #readyCallbacks: CallbackFunction[];
+  #readyArg?: Error = undefined;
+
+  constructor() {
+    this.#isReady = false;
+    this.#readyCallbacks = [];
+  }
+
+  ready(flagOrFunction?: ReadyFunctionArg) {
+    // register a callback
+    if (flagOrFunction === undefined || typeof flagOrFunction === 'function') {
+      return this.#register(flagOrFunction);
+    }
+    // emit callbacks
+    this.#emit(flagOrFunction);
+  }
+
+  /**
+   * Register a callback to the callback stack, it will be called when emit.
+   * It will return promise when no argument passing.
+   */
+  #register(func?: CallbackFunction) {
+    // support `this.ready().then(onready);` and `await this.ready()`;
+    if (!func) {
+      return new Promise<void>((resolve, reject) => {
+        function func(err?: Error) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        }
+        if (this.#isReady) {
+          return func(this.#readyArg);
+        }
+        this.#readyCallbacks.push(func);
+      });
+    }
+
+    // this.ready(fn)
+    if (this.#isReady) {
+      func(this.#readyArg);
+    } else {
+      this.#readyCallbacks.push(func);
+    }
+  }
+
+  /**
+   * Call the callbacks that has been registerd, and clean the callback stack.
+   * If the flag is not false, it will be marked as ready. Then the callbacks will be called immediatly when register.
+   * @param {Boolean|Error} flag - Set a flag whether it had been ready. If the flag is an error, it's also ready, but the callback will be called with argument `error`
+   */
+  #emit(flag: boolean | Error) {
+    // this.ready(true);
+    // this.ready(false);
+    // this.ready(err);
+    this.#isReady = flag !== false;
+    this.#readyArg = flag instanceof Error ? flag : undefined;
+    // this.ready(true)
+    if (this.#isReady) {
+      this.#readyCallbacks
+        .splice(0, Infinity)
+        .forEach(callback => process.nextTick(() => callback(this.#readyArg)));
+    }
+  }
+
+  /**
+   * @param {Object} obj - an object that be mixed
+   */
+  static mixin(obj?: any) {
+    if (!obj) return;
+    const ready = new Ready();
+    // delegate method
+    obj.ready = (flagOrFunction: any) => ready.ready(flagOrFunction);
+  }
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,17 @@
-'use strict';
-
-const assert = require('assert');
-const ready = require('../');
+import { strict as assert } from 'node:assert';
+import Ready, { ReadyFunctionArg } from '../src/index';
 
 class SomeClass {
+  property: string;
+  #readyObject: Ready;
+
   constructor() {
     this.property = 'value';
-    ready.mixin(this);
+    this.#readyObject = new Ready();
+  }
+
+  ready(arg?: ReadyFunctionArg) {
+    return this.#readyObject.ready(arg);
   }
 
   method() {
@@ -14,31 +19,27 @@ class SomeClass {
   }
 }
 
-describe('exports', () => {
+describe('Ready.mixin', () => {
   it('should exports mixin', () => {
-    assert(ready.mixin);
-    assert(ready.mixin === ready);
+    assert(Ready.mixin);
   });
 
   it('should exports Ready', () => {
-    assert(ready.Ready);
+    assert(Ready);
   });
-});
 
-describe('mixin', () => {
   it('should not throw when mixin undefined', () => {
-    ready();
+    Ready.mixin();
   });
 });
 
-describe('inherits', () => {
-
+describe('new Ready()', () => {
   it('should have Ready properties', () => {
     const someClass = new SomeClass();
     assert('ready' in someClass);
   });
 
-  it('should be separate from other instances', function* () {
+  it('should be separate from other instances', async () => {
     const someClass = new SomeClass();
     const anotherClass = new SomeClass();
     let someCallCount = 0;
@@ -48,7 +49,7 @@ describe('inherits', () => {
     someClass.ready(() => { someCallCount++; });
     someClass.ready(true);
     anotherClass.ready(true);
-    yield nextTick();
+    await nextTick();
     assert(someCallCount === 2);
     assert(anotherCallCount === 1);
   });
@@ -56,38 +57,37 @@ describe('inherits', () => {
   it('should ready(obj) directly work', () => {
     const foo = {};
     assert(!('ready' in foo));
-    ready(foo);
-    assert(typeof foo.ready === 'function');
+    Ready.mixin(foo);
+    assert('ready' in foo && typeof foo.ready === 'function');
   });
-});
 
-describe('ready', () => {
-
-  it('should execute and dequeue callbacks', function* () {
+  it('should execute and dequeue callbacks', async () => {
     const someClass = new SomeClass();
-    const arr = [];
+    const arr: number[] = [];
     someClass.ready(() => arr.push(1));
     someClass.ready(() => arr.push(2));
     someClass.ready(true);
-    yield nextTick();
+    await nextTick();
     assert.deepEqual(arr, [ 1, 2 ]);
 
     someClass.ready(true);
-    yield nextTick();
+    await nextTick();
     assert.deepEqual(arr, [ 1, 2 ]);
   });
 
-  it('should immediatly call callback when already ready', function(done) {
+  it('should immediatly call callback when already ready', done => {
     const someClass = new SomeClass();
     someClass.ready(true);
     someClass.ready(done);
   });
 
-  it('should not call when ready set to false', function(done) {
+  it('should not call when ready set to false', done => {
     const someClass = new SomeClass();
     someClass.ready(true);
     someClass.ready(false);
-    someClass.ready(done => done('should not execute because it is not ready'));
+    someClass.ready(() => {
+      done(new Error('should not execute because it is not ready'));
+    });
     setTimeout(() => {
       done();
     }, 10);
@@ -96,27 +96,25 @@ describe('ready', () => {
   it('should ready when using other type', done => {
     const someClass = new SomeClass();
     someClass.ready(done);
-    someClass.ready(0);
+    someClass.ready(0 as any);
   });
 });
 
 describe('promise', () => {
-  it('should resolve after ready', function(done) {
+  it('should resolve after ready', done => {
     const someClass = new SomeClass();
-    someClass.ready().then(() => {
-      someClass.ready().then(done);
+    someClass.ready()!.then(() => {
+      someClass.ready()!.then(done);
     });
     someClass.ready(true);
   });
-});
 
-describe('generator', () => {
-  it('should work with co', function* () {
+  it('should await work', async () => {
     const someClass = new SomeClass();
     setTimeout(() => {
       someClass.ready(true);
     }, 100);
-    yield someClass.ready();
+    await someClass.ready();
   });
 });
 
@@ -133,7 +131,7 @@ describe('error', () => {
 
   it('should get error in promise', done => {
     const someClass = new SomeClass();
-    someClass.ready().catch(err => {
+    someClass.ready()!.catch(err => {
       assert(err);
       assert(err.message === 'error');
       done();
@@ -154,7 +152,7 @@ describe('error', () => {
   it('should get error after ready in promise', done => {
     const someClass = new SomeClass();
     someClass.ready(new Error('error'));
-    someClass.ready().catch(err => {
+    someClass.ready()!.catch(err => {
       assert(err);
       assert(err.message === 'error');
       done();
@@ -163,5 +161,7 @@ describe('error', () => {
 });
 
 function nextTick() {
-  return done => process.nextTick(done);
+  return new Promise<void>(resolve => {
+    process.nextTick(resolve);
+  });
 }
